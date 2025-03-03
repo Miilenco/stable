085de4606ceeb0b676f27414bba86111a6df5a05
Tony Udotong <tonyudotong@gmail.com>
2025-03-03 16:12:16 +0000

    create new horse

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -4,4 +4,24 @@ class HorsesController < ApplicationController
     @horses = Horse.all
   end
 
+  def new
+    @horse = Horse.new
+  end
+
+  def create
+    @horse = Horse.new(horse_params)
+    @horse.user = current_user
+    if @horse.save
+      redirect_to horses_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def horse_params
+    params.require(:horse).permit(:name, :location, :breed, :age, :stud_fee, :pedigry, :progeny_success, :race_record)
+  end
 end
