81b6b750b9d767a025007e252eae950ad6a2fcb1
Hugo French <hugojfrench@icloud.com>
2025-03-04 21:07:37 +0000

    added destroy dependency for bookings on horse model

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,6 +1,6 @@
 class Horse < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, :breed, :location, :stud_fee, presence: true
 end
