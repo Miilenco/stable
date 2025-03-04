e793b34eedd81a964392f02668a5625310ba23e1
Tony Udotong <tonyudotong@gmail.com>
2025-03-04 12:12:06 +0000

    show and edit done

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,8 +1,13 @@
 class HorsesController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :show]
+
   def index
     @horses = Horse.all
   end
 
+  def show
+  end
+  
   def new
     @horse = Horse.new
   end
@@ -17,7 +22,20 @@ class HorsesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @horse.update(horse_params)
+    redirect_to horse_path(@horse)
+  end
+
   private
+
+  def set_task
+    @horse = Horse.find(params[:id])
+  end
+
 
   def horse_params
     params.require(:horse).permit(:name, :breed, :age, :location, :stud_fee, :pedigree, :progeny_success, :race_record)
