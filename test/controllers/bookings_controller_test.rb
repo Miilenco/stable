7611751032378ac5aad6e1f03d101a8848b8cdee
Hugo French <hugojfrench@icloud.com>
2025-03-04 21:19:49 +0000

    removed the wrong test, silly me

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -14,17 +14,17 @@ class BookingsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_horse_booking_url(horses(:one))
-    assert_response :success
-  end
-
   test "should create booking" do
     assert_difference('Booking.count') do
       post bookings_url, params: { booking: { horse_id: @horse.id, user_id: @user.id, start_date: Date.today, end_date: Date.today + 1.week } }
     end
 
     assert_redirected_to booking_url(Booking.last)
+  end
+
+  test "should show booking" do
+    get booking_url(@booking)
+    assert_response :success
   end
 
   test "should update booking" do
