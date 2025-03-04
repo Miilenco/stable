4db540cfd4aa888b006c2896946a6dc92802607b
Hugo French <hugojfrench@icloud.com>
2025-03-04 21:05:42 +0000

    added include devise to booking controller test

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @booking = bookings(:one)
     @horse = horses(:one)
