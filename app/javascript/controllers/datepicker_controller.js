df769bea92b9d87033044562299db49cc63a3568
Hugo French <hugojfrench@icloud.com>
2025-03-06 11:57:03 +0000

    added one to duration, bug fixed

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -28,7 +28,7 @@ export default class extends Controller {
     const endDate = this.endDateTarget.value ? new Date(this.endDateTarget.value) : null;
 
     if (startDate && endDate && endDate >= startDate) {
-      const duration = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24)); // Convert ms to days
+      const duration = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1; // Convert ms to days
       const totalCost = duration * this.priceAtBookingValue;
 
       this.totalCostTarget.value = totalCost.toFixed(2); // Update the total cost input field
