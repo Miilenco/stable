38da670966afd3d696606c0256b5ecb380ed7eb6
Tony Udotong <tonyudotong@gmail.com>
2025-03-06 15:20:19 +0000

    map show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def render_info_window(horse)
+    controller.render_to_string(partial: "horses/info_window", locals: { horse: horse })
+  end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -4,5 +4,8 @@ class Horse < ApplicationRecord
 
   has_many_attached :pictures
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   validates :name, :breed, :location, :stud_fee, presence: true
 end
