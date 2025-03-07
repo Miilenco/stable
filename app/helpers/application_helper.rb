263385dabbe43682a50c5c1033ea68b949d3c16b
Tony Udotong <tonyudotong@gmail.com>
2025-03-07 14:26:19 +0000

    map on index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def render_info_window(horse)
+    return "" if horse.nil?
     controller.render_to_string(partial: "horses/info_window", locals: { horse: horse })
   end
 end
+
